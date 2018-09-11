d77dc4794401d319f1c0cf62bf15eb02e4cdd42d
Frederik Sloth Stræde <frederiksloths@gmail.com>
2018-09-11 11:55:33 +0200

    Metode givDosis i PN er lavet (linje 25)

--- src/ordination/PN.java
+++ src/ordination/PN.java
@@ -1,33 +1,45 @@
 package ordination;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
 
 public class PN extends Ordination {
 
-    public PN(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
-		super(startDen, slutDen, laegemiddel);
-	}
+    private ArrayList<LocalDate> doserGivet = new ArrayList<>();
 
-	private double antalEnheder;
+    public PN(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
+        super(startDen, slutDen, laegemiddel);
+    }
+
+    private double antalEnheder;
 
     /**
-     * Registrerer at der er givet en dosis paa dagen givesDen
-     * Returnerer true hvis givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
+     * Registrerer at der er givet en dosis paa dagen givesDen Returnerer true hvis
+     * givesDen er inden for ordinationens gyldighedsperiode og datoen huskes
      * Retrurner false ellers og datoen givesDen ignoreres
+     *
      * @param givesDen
      * @return
      */
     public boolean givDosis(LocalDate givesDen) {
-        // TODO
-        return false;   
+        // Checker om dateon er inden for doserings datoerne, og tilføjer dato for dosis
+        // til et ArrayList (doserGivet)
+        if (givesDen.isAfter(getStartDen()) && givesDen.isBefore(getSlutDen())) {
+            doserGivet.add(givesDen);
+            return true;
+        } else {
+            return false;
+        }
     }
 
+    @Override
     public double doegnDosis() {
         // TODO
         return 0.0;
     }
 
-
+    @Override
     public double samletDosis() {
         // TODO
         return 0.0;
@@ -35,21 +47,22 @@ public class PN extends Ordination {
 
     /**
      * Returnerer antal gange ordinationen er anvendt
+     *
      * @return
      */
     public int getAntalGangeGivet() {
         // TODO
-        return-1;
+        return -1;
     }
 
     public double getAntalEnheder() {
         return antalEnheder;
     }
 
-	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 }
