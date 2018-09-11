732b152000cd6533e0dcdd36963352e41d4eaa09
Michael <Inztagib@hotmail.com>
2018-09-11 11:35:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/ordination/DagligFast.java
+++ src/ordination/DagligFast.java
@@ -5,31 +5,36 @@ import java.util.Arrays;
 
 public class DagligFast extends Ordination {
 
-	private Dosis[] doser = new Dosis[4];
-	
-	public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
-		super(startDen, slutDen, laegemiddel);
-	}
-	
-	public Dosis[] getDoser(){
-		return Arrays.copyOf(doser, 4);
-	}
-	
-	@Override
-	public double samletDosis() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    private Dosis[] doser = new Dosis[4];
 
-	@Override
-	public double doegnDosis() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public DagligFast(LocalDate startDen, LocalDate slutDen, Laegemiddel laegemiddel) {
+        super(startDen, slutDen, laegemiddel);
+    }
 
-	@Override
-	public String getType() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    public Dosis[] getDoser() {
+        return Arrays.copyOf(doser, 4);
+    }
+
+    @Override
+    public double samletDosis() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public double doegnDosis() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public Dosis[] getDoser() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
