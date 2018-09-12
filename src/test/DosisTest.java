ddf79eb1c44effcce557399a6d5ef13dcefb8215
Frederik Sloth Stræde <frederiksloths@gmail.com>
2018-09-12 10:56:37 +0200

    Create test package Moved DosisTest to it

---
+++ src/test/DosisTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.time.LocalTime;
+
+import org.junit.Test;
+
+import ordination.Dosis;
+
+public class DosisTest {
+
+    @Test
+    public void TC1() {
+        Dosis d = new Dosis(0);
+        d.setTid(LocalTime.of(00, 00));
+        assertEquals(LocalTime.of(00, 00), d.getTid());
+    }
+
+    @Test
+    public void TC2() {
+        Dosis d = new Dosis(0);
+        d.setTid(LocalTime.of(12, 00));
+        assertEquals(LocalTime.of(12, 00), d.getTid());
+    }
+
+    @Test
+    public void TC3() {
+        Dosis d = new Dosis(0);
+        d.setTid(LocalTime.of(23, 59));
+        assertEquals(LocalTime.of(23, 59), d.getTid());
+    }
+
+    @Test
+    public void TC4() {
+        Dosis d = new Dosis(0);
+        d.setTid(null);
+        assertEquals(null, d.getTid());
+
+    }
+
+}
