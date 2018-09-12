9ef3d43dc71461c8dfbea3e33eb65ff3bebaeb0c
Frederik Sloth Stræde <frederiksloths@gmail.com>
2018-09-12 10:40:39 +0200

    Create DosisTest

---
+++ src/ordination/DosisTest.java
@@ -0,0 +1,14 @@
+package ordination;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DosisTest {
+
+    @Test
+    public void TC1() {
+        fail("Not yet implemented");
+    }
+
+}
