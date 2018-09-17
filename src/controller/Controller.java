73d9f4c7b302ac88536474194c35bdb704c4fef6
Fillipuster <jonas@priestyard.dk>
2018-09-17 10:42:16 +0200

    Added proper argument exception handling to opretDagligFastOrdination

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -65,12 +65,16 @@ public class Controller {
     public DagligFast opretDagligFastOrdination(LocalDate startDen, LocalDate slutDen, Patient patient,
             Laegemiddel laegemiddel, double morgenAntal, double middagAntal, double aftenAntal, double natAntal) {
         if (controller.checkStartFoerSlut(startDen, slutDen)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("startDen skal være før slutDen");
         }
         DagligFast df = new DagligFast(startDen, slutDen, laegemiddel);
 
         if (startDen == null || slutDen == null || laegemiddel == null || patient == null) {
-            throw new IllegalArgumentException("NULL argumenter ikke accepteret");
+            throw new IllegalArgumentException("NULL argumenter ikke accepteret.");
+        }
+        
+        if (morgenAntal <= 0 || middagAntal <= 0 || aftenAntal <= 0 || natAntal <= 0) {
+        	throw new IllegalArgumentException("Ingen antal må være 0 eller under.");
         }
 
         df.createDosis(Dagstidspunkt.MORGEN, morgenAntal);
